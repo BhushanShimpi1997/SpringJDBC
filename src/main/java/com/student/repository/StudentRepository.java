d1fc7438ad8b900993fbe426346fd1e4e4e12df8
BhushanShimpi <bhushanshimpi2816@gmail.com>
2023-05-30 18:02:15 +0530

    Using NamedParameterJDBC Template

--- src/main/java/com/student/repository/StudentRepository.java
+++ src/main/java/com/student/repository/StudentRepository.java
@@ -5,11 +5,16 @@ import com.student.rowmapper.StudentRowMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -18,7 +23,7 @@ import java.util.Optional;
 public class StudentRepository {
 
     private JdbcTemplate template;
-
+    private NamedParameterJdbcTemplate namedTemplate;
     //create Student
     public int createStudent(Student student){
         student.setCreatedAt(LocalDateTime.now());
@@ -33,29 +38,38 @@ public class StudentRepository {
     public List<Student> getAll() {
         String sql="select * from student";
         log.info("executing Query:{}",sql);
-        List<Student> result = template.query(sql, new StudentRowMapper());
+        final List<Student> result = namedTemplate.query(sql, new StudentRowMapper());
         log.info("response from DB:{}",result);
         return result;
     }
 
     public Optional<Student> getStudentByRollNo(int rollNo){
-        String sql="select * from student where rollNo=?";
-        List<Student> query = template.query(sql, new StudentRowMapper(), rollNo);
+        String sql="select * from student where rollNo=:roll";
+        SqlParameterSource parameterSource=new MapSqlParameterSource("roll",rollNo);
+        final List<Student> query = namedTemplate.query(sql, parameterSource, new StudentRowMapper());
         Optional<Student> student = query.stream().findFirst();
         return student;
     }
 
     public Student updateStudentByRollNo(Student student,int rollNo){
-        String query="update student set studentName=?,mobileNumber=?,city=?,updatedAt=? where rollNo=?";
-        int result = template.update(query, student.getStudentName(), student.getMobileNumber(),
-                                            student.getCity(), student.getUpdatedAt(), rollNo);
+        String query="update student set studentName=:name,mobileNumber=:number," +
+                     "city=:city,updatedAt=:update where rollNo=:roll";
+        Map<String,Object> paramList=new LinkedHashMap<>();
+        paramList.put("name",student.getStudentName());
+        paramList.put("number",student.getMobileNumber());
+        paramList.put("city",student.getCity());
+        paramList.put("update",student.getUpdatedAt());
+        paramList.put("roll",rollNo);
+        SqlParameterSource parameterSource=new MapSqlParameterSource(paramList);
+        final int result = namedTemplate.update(query, parameterSource);
         log.info("rows Affected:{}",result);
         return student;
     }
 
     public int delete(int rollNo){
-        String sql="DELETE from STUDENT where rollNo=?";
-        int row = template.update(sql, rollNo);
+        String sql="DELETE from STUDENT where rollNo=:roll";
+        SqlParameterSource parameterSource=new MapSqlParameterSource("roll",rollNo);
+        final int row = namedTemplate.update(sql, parameterSource);
         return row;
     }
 }
